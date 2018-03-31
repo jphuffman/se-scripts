46fb8c46b6ccaeccb6be4e7bbcabf5ec0ecfe5a1
MistaLemur <d4rk3.54b3r@gmail.com>
2018-03-30 18:32:58 -0700

    Fixed compiler error on Whip's Rotor Controller.cs

---
+++ Approved Scripts/Whip's Rotor Controller/Whip's Rotor Controller.cs
@@ -0,0 +1,12 @@
+```
+Word is, the late privateering Captain Talon the Stubborn has stashed away his wealth on one of the nearby isles.
+
+From the first isle of bountiful resources, travel 6 leagues northeast to the Rato e Águia Islands.
+Then travel another 6 leagues west to reach the Isle of Three.
+Travel 4 leagues north, and you will reach the Abysmal Reef.
+The treasure lies upon the northwestern-most island of the Abysmal Reef!
+
+The first faction to redeem this treasure, with photographic evidence, at the central base of dropoff shall be awarded 5 Organic Points!
+You must immediately take the treasure to the central base when you first collect it!
+Be wary: Captain Talon was quite wealthy! Your vessel must be capable of lifting his heavy stash (70 tons)!
+```
